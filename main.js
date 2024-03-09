37fedd8186d0c6b8089119a15096baabc402ede9
Lisa <md.nuralisa@gmail.com>
2024-03-09 21:56:40 +0800

    update list

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const fetchAllTodo = () => {
             ${todo.details}
             <div class="d-flex" >
               <button class="btn btn-${todo.completed == 1 ? 'success' : 'warning'} me-1">${todo.completed == 1 ? '<i class="bi bi-check"></i>' : '<i class="bi bi-x"></i>'}</button>
-              <button class='btn btn-primary me-1' data-bs-toggle="modal" data-bs-target="#editTodo" ><i class="bi bi-pencil"></i></button>
+              <button class='btn btn-primary me-1' data-bs-toggle="modal" data-bs-target="#editTodo" onclick = 'selectTodo(${JSON.stringify(todo)})' ><i class="bi bi-pencil"></i></button>
               <button class='btn btn-danger'><i class="bi bi-trash"></i></button>
             </div>
           </div>`
@@ -57,4 +57,47 @@ const selectTodo = (todo) => {
     selectedTodo = todo
 }
 
-fetchAllTodo()
+let isChecked = false;
+const checked =() => {document.addEventListener('DOMContentLoaded', function() {
+    const checkBox = document.getElementById('checkBox');
+    const incompleteLabel = document.querySelector('.Incomplete');
+    const completedLabel = document.querySelector('.Completed');
+    
+    checkBox.addEventListener('change', function() {
+      isChecked = checkBox.checked;
+      if (isChecked) {
+        incompleteLabel.style.display = 'none';
+        completedLabel.style.display = 'block';
+      } else {
+        incompleteLabel.style.display = 'block';
+        completedLabel.style.display = 'none';
+      }
+      console.log(isChecked)
+    });
+  })
+  }
+
+const onSaveChanges = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    const inputValue = document.getElementById('todoUpdate').value
+    fetch(`https://api.kelasprogramming.com/todo/${selectedTodo.id}`,{
+        method: 'PUT',
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${JWTtoken}`
+        },
+        body: JSON.stringify({
+            "details": inputValue,
+            "completed": isChecked ? 1 : 0
+        })
+    })
+    .then(res => res.json())
+    .then(body =>{
+        fetchAllTodo()
+        document.getElementById('todoUpdate').value = ''
+        document.getElementById('closeModal').click()
+    })
+}
+
+fetchAllTodo();
+checked();
